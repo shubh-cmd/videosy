247cf90e2cf4105f48a87ef8c64489268084dd94
shubh-cmd <krshubham878@gmail.com>
2022-09-05 17:20:19 +0530

    initial commit

--- videosy/settings.py
+++ videosy/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if env('ENVIRONMENT') == 'DEV' else False
 
-ALLOWED_HOSTS = ['daphne.herokuapp.com', '20.219.109.231']
+ALLOWED_HOSTS = ['videosy.herokuapp.com', '20.219.109.231']
 
 
 # Application definition
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
-    'https://daphne.herokuapp.com'
+    'https://videosy.herokuapp.com'
 ]
 
 django_heroku.settings(locals())
