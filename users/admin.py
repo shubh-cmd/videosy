d1a797ce05a834abd65e406b0f4e341733984e43
shubh-cmd <krshubham878@gmail.com>
2022-09-05 17:24:10 +0530

    initial commit

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import PasswordReset, User
+
+@admin.register(User)
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'id']
+
+
+admin.site.register(PasswordReset)
